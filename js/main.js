97fc906fd08aad576930f7077a9019e51f241c56
Alex Turcu <turcu.alex5@gmail.com>
2017-08-03 17:57:54 +0300

    gg

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
+let countdown
+const timeDisplay = document.querySelector(".timer")
+let value = document.querySelectorAll(".value");
+let control = document.querySelectorAll(".control")
 
+function timer(seconds){
+  //clear any existing timers
+  clearInterval(countdown);
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now())/ 1000)
+    if(secondsLeft < 0){
+      clearInterval(countdown);
+      return
+
+    }
+    displayTimeLeft(secondsLeft)
+  }, 1000)
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? "0" : ""}${remainderSeconds}`;
+  timeDisplay.textContent = display;
+  document.title = display;
+
+}
+
+function setValue(){
+  if(this.textContent == "+"){
+    this.previousSibling.previousSibling.innerHTML ++;
+  } else if( this.textContent == "-"){
+    this.nextSibling.nextSibling.innerHTML --;
+  }
+
+}
+function getValue() {
+  const seconds = (parseInt(this.innerHTML)) * 1000;
+
+  timer(seconds)
+}
+
+
+control.forEach(controle => controle.addEventListener("click", setValue));
+value.forEach(val => val.addEventListener("click", getValue));
