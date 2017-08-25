b692c112ae9c29a99d4a1496f7716dd2e5b356be
Alex Turcu <turcu.alex5@gmail.com>
2017-08-25 11:00:23 +0300

    final v w/ alarm

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
+
 let countdown
 const timeDisplay = document.querySelector(".timer")
-let value = document.querySelectorAll(".value");
-let control = document.querySelectorAll(".control")
+const stopAudio = document.querySelector(".stopAudio")
 const ready = document.querySelector(".pomodoro p")
+const pomodoro = document.querySelector(".pomodoro")
 let flag = false;
 const reset = document.querySelector(".reset")
+const input = document.querySelectorAll("input")
+const audio = document.querySelector("audio")
 
 
 function timer(seconds) {
@@ -17,6 +20,11 @@ function timer(seconds) {
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     if(secondsLeft < 0) {
+      audio.loop = false;
+      audio.play()
+      ready.innerHTML="Start"
+      pomodoro.style.borderColor = "#1E521E";
+      pomodoro.style.color = "#1E521E";
       clearInterval(countdown);
       return;
     }
@@ -34,49 +42,46 @@ function displayTimeLeft(seconds) {
 
 }
 
-function setValue(){
-  if(this.textContent == "+"){
 
-    timeDisplay.textContent = this.previousSibling.previousSibling.innerHTML ++ +":00";
-  } else if( this.textContent == "-"){
-    timeDisplay.textContent = this.nextSibling.nextSibling.innerHTML --  +":00";
-  }
 
-}
-
-function getValue() {
+function startValue() {
   flag = !flag
-
-
-  const string = this.textContent.split(":")
-
+  const string = timeDisplay.textContent.split(":")
   const seconds = parseInt(string[0]) * 60 + parseInt(string[1]);
-
   if(flag == true) {
     timer(seconds);
     ready.innerHTML="Stop"
-
-
+    pomodoro.style.borderColor = "red";
+    pomodoro.style.color = "red";
   } else {
-
     clearInterval(countdown);
     ready.innerHTML="Start"
-
-
+    pomodoro.style.borderColor = "#1E521E";
+    pomodoro.style.color = "#1E521E";
   }
-
-
 }
 
 function resetValue() {
   clearInterval(countdown);
   timeDisplay.innerHTML = "25:00"
-  ready.innerHTML = "start"
+  ready.innerHTML = "Start"
+  flag = false;
+  pomodoro.style.borderColor = "#1E521E";
+  pomodoro.style.color = "#1E521E";
+}
+
+function getValues () {
+
+  timeDisplay.innerHTML = this.value + ":00"
+}
+function stopAudioEvent () {
+  audio.pause();
+  audio.currentTime = 0;
 }
 
 
 
-
-control.forEach(controle => controle.addEventListener("click", setValue));
-timeDisplay.addEventListener("click", getValue);
+pomodoro.addEventListener("click", startValue);
 reset.addEventListener("click", resetValue);
+input.forEach(inputs => inputs.addEventListener("change", getValues));
+stopAudio.addEventListener("click", stopAudioEvent)
